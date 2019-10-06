13fc0a3ca0e30dd105eb5def522f4a38184a3a59
Taufikhdyt17 <40555671+Taufikhdyt17@users.noreply.github.com>
2019-10-06 20:29:40 +0800

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -1,6 +1,7 @@
 num1 = int(input('Masukkan bilangan pertama: '))
 num2 = int(input('Masukkan bilangan kedua: '))
-num3 = int(input('Masukkan bilangan kedua: '))
+num3 = int(input('Masukkan bilangan ketiga: '))
+num4 = int(input('Masukkan bilangan keempat: '))
 
-Hasil= (num1 + num2 + num3)
+Hasil= (num1 + num2 + num3 + num4)
 print(Hasil)
